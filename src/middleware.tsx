f02546db120b96ae8f757ee5ae3dfb1c3c1b08bc
Muhammad Usman <usmanG@arkitektz.com>
2023-10-12 22:45:16 +0500

    fixed errors

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,23 +3,27 @@ import type { NextRequest } from "next/server";
 import { stripe } from "@/lib/stripe";
 
 export async function middleware(request: NextRequest, response: NextResponse) {
-  const session = request.cookies.get("session");
+  try {
+    const session = request.cookies.get("session");
 
-  // Return to /login if don't have a session
-  if (!session) {
-    return NextResponse.redirect(new URL("/", request.url));
+    // Return to /login if don't have a session
+    if (!session) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    const stripeCustomer = request.cookies.get("stripe");
+
+    const subscriptions = await stripe.subscriptions.list({
+      customer: stripeCustomer?.value,
+      limit: 3,
+    });
+
+    if (subscriptions?.data?.length > 0) return NextResponse.next();
+
+    return NextResponse.redirect(new URL("/payment", request.url));
+  } catch (err) {
+    return NextResponse.redirect(new URL("/payment", request.url));
   }
-
-  const stripeCustomer = request.cookies.get("stripe");
-
-  const subscriptions = await stripe.subscriptions.list({
-    customer: stripeCustomer?.value,
-    limit: 3,
-  });
-
-  if (subscriptions?.data?.length > 0) return NextResponse.next();
-
-  return NextResponse.redirect(new URL("/payment", request.url));
 }
 
 // Add your protected routes
